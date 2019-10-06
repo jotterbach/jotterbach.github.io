ec4802f0de6fd91e9f9603baaa66c42cbd7d6af8
Johannes Otterbach <johannesotterbach@gmail.com>
2019-10-06 13:17:16 -0700

    Move layout to styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,16 +6,63 @@ import {
   faGithub,
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons"
-import "./layout.css"
+import styled from "styled-components"
+
+
+const Layout = styled.div`
+  margin: 3rem auto;
+  max-width: 900px;
+  padding: 0 1rem;
+
+  display: flex;
+  min-height: 0vh;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+`
+
+const Head = styled.header`
+  margin-bottom: 1.5rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  justify-items: center;
+
+`
+
+const NavBarLink = styled.li`
+  display: inline-block;
+  margin-right: 1rem;
+  a {
+    color: darkblue;
+    text-decoration: none;
+  }
+`
+
+const FooterLink = styled.div`
+  margin-left: 5px;
+  margin-right: 5px;
+  text-align: "center";
+`
+
+const Foot = styled.footer`
+  margin-top: 2.5rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  justify-items: center;
+  flex-wrap: wrap;
+`
 
 const ListLink = props => (
-  <li className="NavBarLink">
+  <NavBarLink>
     <Link to={props.to}>{props.children}</Link>
-  </li>
+  </NavBarLink>
 )
 
 const Header = () => (
-  <header className="Header">
+  <Head>
     <div>
       <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
         <h3 style={{ display: `inline-block` }}>Johannes Otterbach</h3>
@@ -27,31 +74,31 @@ const Header = () => (
         <ListLink to="/professional/">Professional</ListLink>
       </ul>
     </div>
-  </header>
+  </Head>
 )
 
 const Footer = () => (
-  <footer className="Footer">
-    <div className="FooterLink">
+  <Foot>
+    <FooterLink>
       <a href="https://linkedin.com/in/jotterbach" alt="linkedin">
         <FontAwesomeIcon icon={faLinkedinIn} size={`2x`} color={`#606060`} />
       </a>
-    </div>
-    <div className="FooterLink">
+    </FooterLink>
+    <FooterLink>
       <a href="https://www.github.com/jotterbach" alt="github">
         <FontAwesomeIcon icon={faGithub} size={`2x`} color={`#606060`} />
       </a>
-    </div>
-    <div className="FooterLink">
+    </FooterLink>
+    <FooterLink>
       <a href="https://www.twitter.com/jsotterbach" alt="twitter">
         <FontAwesomeIcon icon={faTwitter} size={`2x`} color={`#606060`} />
       </a>
-    </div>
-  </footer>
+    </FooterLink>
+  </Foot>
 )
 
 export default ({ children }) => (
-  <div className="Layout">
+  <Layout>
     <div>
       <Header />
     </div>
@@ -59,5 +106,5 @@ export default ({ children }) => (
     <div>
       <Footer />
     </div>
-  </div>
+  </Layout>
 )
